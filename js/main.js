7f6cb5a43ad13ed2527ebf277572bca80f420604
alicjaada83 <alicja.adamczyk83@gmail.com>
2023-02-20 19:43:03 +0100

    odpowiedz1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 17;
+let num2 =20;
+let numsuma = num1 + num2;
+if (num1 === 20) {
+    console.log(true);
+} else if (num2 === 20) {
+    console.log(true);
+} else if (numsuma <= 20) {
+    console.log(true);
+} 
+else {
+    console.log(false);
+}
